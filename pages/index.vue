3497550dd23d900c6fa669633e7d3f211e81c595
Marcus Christiansen <christiansen.marcus@gmail.com>
2020-03-29 21:38:40 +0200

    add status and stats

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,17 @@
         <img src="~/assets/logo.png" class="logo" alt="Is Covid 19 Cured">
       </a-layout-header>
       <a-layout-content>
-        <h1 class=text-center>{{ date }}</h1>
+        <h1 class="text-center">{{ date }}</h1>
+        <a-row class="text-center" style="margin-bottom:2rem;">
+          <a-col :md="{ span: 12, offset: 6 }">
+            <span v-if="statistics">
+              <a-tag>Cases: {{ formatNumber(statistics.data.confirmed) }}</a-tag>
+              <a-tag color="#2db7f5">Active: {{ formatNumber(statistics.data.active) }}</a-tag>
+              <a-tag color="#f50">Deaths: {{ formatNumber(statistics.data.deaths) }}</a-tag>
+              <a-tag color="#87d068">Recovered: {{ formatNumber(statistics.data.recovered) }}</a-tag>
+            </span>
+          </a-col>
+        </a-row>
         <a-row>
           <a-col :md="{ span: 12, offset: 6 }">
             <a-card v-for="(article, key) in newsArticles" :key="key" style="margin-bottom:2rem;">
@@ -33,11 +43,14 @@ import axios from 'axios'
 export default {
   mounted() {
     this.fetchNewsArticles()
+    this.fetchStatusUpdate()
+    this.fetchStatistics()
   },
   data() {
     return {
       year: this.$moment().format('YYYY'),
       date: this.$moment().format('MMMM Do YYYY'),
+      statistics: null,
       searchString: 'Covid-19 Cure',
       newsArticles: []
     }
@@ -50,6 +63,33 @@ export default {
       await axios
         .get(`https://newsapi.org/v2/everything?qInTitle=${this.searchString}&sortBy=publishedAt&apiKey=6cc79d1255a44ad0b79a346ef065cf3d`)
         .then(response => (this.newsArticles = response.data.articles))
+    },
+    async fetchStatusUpdate() {
+      this.statusCureNotFound()
+    },
+    async fetchStatistics() {
+      await axios
+        .get('https://covid2019-api.herokuapp.com/v2/total')
+        .then(response => (this.statistics = response.data))
+    },
+    statusCureFound() {
+      return this.$notification.success({
+          message: 'Status Update - CURE FOUND',
+          description:
+            'A cure has been found for Covid-19.',
+          duration: 0
+      });
+    },
+    statusCureNotFound() {
+      return this.$notification.error({
+          message: 'Status Update - NO CURE FOUND',
+          description:
+            'At this stage no cure treatment is available to treat Covid-19.',
+          duration: 0
+      });
+    },
+    formatNumber(num) {
+      return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
     }
   },
 }
